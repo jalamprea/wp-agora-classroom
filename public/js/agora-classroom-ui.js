dfa7e711651c39dff804806e7f40d200de8728c7
Julian Lamprea <jalamprea@gmail.com>
2020-06-27 19:18:50 -0500

    Refactor and clean code on swap videos functions

--- public/js/agora-classroom-ui.js
+++ public/js/agora-classroom-ui.js
@@ -1,7 +1,10 @@
 
+/*
+ * Event Listener on Double Click on remote streams (students divs).isMainHost
+ *
+ * Behavior: play selected container as full screen - swap out current full screen stream
+ */
 function swapVideoStudentAndHost() {
-  // play selected container as full screen - swap out current full screen stream
-  // console.log('Double click!');
 
   if (!RTC.hostJoined && !window.isMainHost) {
     console.log('no host joined... action ignored');
@@ -11,12 +14,14 @@ function swapVideoStudentAndHost() {
   const uid = this.id.replace('remote-container-', '');
   const videoPlayer = jQuery(this).find('video');
   let streamIdWithSuffix = videoPlayer[0].id.replace('video', '');
-  // console.log(streamIdWithSuffix);
+  
+  const mainHostStream = window.isMainHost ? RTC.localStreams.cam1.stream : RTC.remoteStreams[window.hostID];
+  const mainPlayerID = window.isMainHost ? 'local-video-cam1' : 'host-video-' + window.hostID;
 
   // if this container is playing a student video:
   if (streamIdWithSuffix.indexOf(uid)===0) {
     if (window.isMainHost) {
-      let mainVideoId = document.getElementById('local-video-cam1').children[0].id;
+      let mainVideoId = document.getElementById(mainPlayerID).children[0].id;
       if (mainVideoId==='player_' + window.hostID) {
         swapStudentToTeacher(uid, streamIdWithSuffix);
       } else {
@@ -27,10 +32,10 @@ function swapVideoStudentAndHost() {
       }
     } else {
       const mainHostPlayer = document.getElementById('player_' + window.hostID);
-      if (mainHostPlayer.parentElement.id==='host-video-' + window.hostID) {
+      if (mainHostPlayer.parentElement.id===mainPlayerID) {
         swapStudentToTeacher(uid, streamIdWithSuffix);
       } else {
-        mainVideoId = document.getElementById('host-video-' + window.hostID).children[0].id.replace('player_', '');
+        mainVideoId = document.getElementById(mainPlayerID).children[0].id.replace('player_', '');
         const currentUid = window.AGORA_UTILS.getRealUserId(mainVideoId);
         swapStudents(uid, streamIdWithSuffix, currentUid, mainVideoId);
         window.AGORA_UTILS.toggleVisibility('#'+currentUid+'_username', true);
@@ -45,93 +50,84 @@ function swapVideoStudentAndHost() {
     // disable .username-overlay
     window.AGORA_UTILS.toggleVisibility('#'+uid+'_username', true);
   }
-}
 
-// Restore main host to be on main player
-function swapMainHostToStudent(uid) {
-  const mainHostStream = window.isMainHost ? RTC.localStreams.cam1.stream : RTC.remoteStreams[window.hostID];
-  const mainPlayerID = window.isMainHost ? 'local-video-cam1' : 'host-video-'+window.hostID;
-  const streamIdWithSuffix = document.getElementById(mainPlayerID).children[0].id.replace('player_', '');
-  
-  mainHostStream.stop();
-  RTC.studentsDouble[uid][streamIdWithSuffix].stop();
-  RTC.studentsDouble[uid][streamIdWithSuffix].play('agora_remote_' + uid);
+  // Restore main host to be on main player
+  function swapMainHostToStudent(uid) {
+    const streamIdWithSuffix = document.getElementById(mainPlayerID).children[0].id.replace('player_', '');
+    const clickedStudentStream = RTC.studentsDouble[uid][streamIdWithSuffix];
+    
+    mainHostStream.stop();
+    clickedStudentStream.stop();
+    clickedStudentStream.play('agora_remote_' + uid);
 
-  mainHostStream.play(mainPlayerID);
+    mainHostStream.play(mainPlayerID);
 
-  if (window.isMainHost) {
-    if (RTC.localStreams.cam2 && RTC.localStreams.cam2.stream && RTC.localStreams.cam2.stream.play) {
-      RTC.localStreams.cam2.stream.play('local-video-cam2');
-      jQuery('#local-video-cam2').show();
-    }
-  } else {
-    const idCam2 = window.hostID * (window.hostID + 123);
-    if (RTC.remoteStreams[idCam2]) {
-      RTC.remoteStreams[idCam2].play('host-video-'+idCam2);
-      jQuery('#host-video-'+idCam2).show();
+    if (window.isMainHost) {
+      if (RTC.localStreams.cam2 && RTC.localStreams.cam2.stream && RTC.localStreams.cam2.stream.play) {
+        RTC.localStreams.cam2.stream.play('local-video-cam2');
+        document.getElementById('local-video-cam2').style.display = 'block';
+      }
+    } else {
+      const idCam2 = window.hostID * (window.hostID + 123);
+      if (RTC.remoteStreams[idCam2]) {
+        RTC.remoteStreams[idCam2].play('host-video-'+idCam2);
+        document.getElementById('host-video-'+idCam2).style.display = 'block';
+      }
     }
   }
-}
 
-// move student to the main player and mian host video into the student div
-function swapStudentToTeacher(uid, streamIdWithSuffix) {
+  // move student to the main player and main host video into the student div
+  function swapStudentToTeacher(uid, streamIdWithSuffix) {
 
-  const clickedStudentStream = RTC.studentsDouble[uid][streamIdWithSuffix];
-  clickedStudentStream.stop();
+    const clickedStudentStream = RTC.studentsDouble[uid][streamIdWithSuffix];
+    clickedStudentStream.stop();
 
-  if (window.isMainHost) {
-    RTC.localStreams.cam1.stream.stop();
-    if (RTC.localStreams.cam2 && RTC.localStreams.cam2.stream && RTC.localStreams.cam2.stream.stop) {
-      RTC.localStreams.cam2.stream.stop();
-      jQuery('#local-video-cam2').hide();
-    }
+    mainHostStream.stop();
 
-    // play remote student on the main host container
-    clickedStudentStream.play('local-video-cam1');
-
-    // play main host on the student box
-    RTC.localStreams.cam1.stream.play('agora_remote_'+uid);
-  } else {
-    // streamId===window.hostID || streamId===(window.hostID * (window.hostID + 123))
-    if (RTC.remoteStreams[window.hostID]) {
-      RTC.remoteStreams[window.hostID].stop();
+    if (window.isMainHost) {
+      if (RTC.localStreams.cam2 && RTC.localStreams.cam2.stream && RTC.localStreams.cam2.stream.stop) {
+        RTC.localStreams.cam2.stream.stop();
+        document.getElementById('local-video-cam2').style.display = 'none';
+      }
+    } else {
       const idCam2 = window.hostID * (window.hostID + 123);
       if (RTC.remoteStreams[idCam2]) {
         RTC.remoteStreams[idCam2].stop();
-        jQuery('#host-video-'+idCam2).hide();
+        document.getElementById('host-video-'+idCam2).style.display = 'none';
       }
-
-      clickedStudentStream.play('host-video-'+window.hostID);
-      RTC.remoteStreams[window.hostID].play('agora_remote_'+uid);
     }
+
+    // play remote student on the main host container
+    clickedStudentStream.play(mainPlayerID);
+
+    // play main host on the student box
+    mainHostStream.play('agora_remote_'+uid);
   }
-}
 
+  // change main player from one student to another student
+  function swapStudents(newUid, newStreamIdWithSuffix, currentUid, currentStreamId) {
+    const clickedStudentStream = RTC.studentsDouble[newUid][newStreamIdWithSuffix];
+    const currentMainStudentStream = RTC.studentsDouble[currentUid][currentStreamId];
 
-function swapStudents(newUid, newStreamIdWithSuffix, currentUid, currentStreamId) {
-  // console.log(newUid, newStreamIdWithSuffix);
-  // console.log(currentUid, currentStreamId);
-  const clickedStudentStream = RTC.studentsDouble[newUid][newStreamIdWithSuffix];
-  const currentMainStudentStream = RTC.studentsDouble[currentUid][currentStreamId];
+    // stop new clicked student
+    clickedStudentStream.stop();
 
-  // stop new clicked student
-  clickedStudentStream.stop();
+    // stop current student (the one on the main player)
+    currentMainStudentStream.stop();
 
-  // stop current student (the one on the main player)
-  currentMainStudentStream.stop();
+    // stop mainHost
+    mainHostStream.stop();
 
-  // stop mainHost
-  const mainHostStream = window.isMainHost ? RTC.localStreams.cam1.stream : RTC.remoteStreams[window.hostID];
-  mainHostStream.stop();
+    // play new student on Main player
+    clickedStudentStream.play(mainPlayerID);
 
-  // play new student on Main player
-  window.isMainHost ? clickedStudentStream.play('local-video-cam1') : clickedStudentStream.play('host-video-'+window.hostID);
+    // play current student on the original div
+    currentMainStudentStream.play('agora_remote_' + currentUid);
 
-  // play current student on the original div
-  currentMainStudentStream.play('agora_remote_' + currentUid);
+    // play mainHost on the new student div
+    mainHostStream.play('agora_remote_' + newUid);
+  }
 
-  // play mainHost on the new student div
-  mainHostStream.play('agora_remote_' + newUid);
 }
 
 // EOF
