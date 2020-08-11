f1c2c82a694db897eea5f41bbed1e6b762f7c17e
Julian Lamprea <jalamprea@gmail.com>
2020-08-11 10:02:57 -0500

    Fixed camera 2 validation when saving settings on students side

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 **Requires at least:** 5.0.0  
 **Tested up to:** 5.4  
 **Requires PHP:** 7.1
-**Stable tag:** 1.0.5
+**Stable tag:** 1.0.6
 **License:** CC BY-ND 4.0    
 **License URI:** https://creativecommons.org/licenses/by-nd/4.0  
 **GitHub:** https://github.com/jalamprea/wp-agora-classroom
--- public/js/communication-ui.js
+++ public/js/communication-ui.js
@@ -137,7 +137,10 @@ window.AGORA_COMMUNICATION_UI = {
 
       try {
           RTC.localStreams.cam1.device = window.availableCams.find(cam => cam.deviceId===cam1);
-          RTC.localStreams.cam2.device = window.availableCams.find(cam => cam.deviceId===cam2);
+
+          if (cam2) {
+            RTC.localStreams.cam2.device = window.availableCams.find(cam => cam.deviceId===cam2);
+          }
           
           const newCamsArray = [RTC.localStreams.cam1.device, RTC.localStreams.cam2.device];
 
